4d41a04978af7f11cbefe8d341e5901455506865
sebastianff <sebastianffarago@gmail.com>
2015-11-24 22:28:08 +0100

    Added firebase and diplay for the events

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+var myDataRef = new Firebase('https://shining-fire-6551.firebaseio.com/');
+
+var getInfo = function(){
+	var name = $('#name').val();
+	var email = $('#email').val();
+	var pass = $('#pass').val();
+	var eventName = $('#eventName').val();
+	var eventType = $('#eventType').val();
+	var eventStart = $('#eventStart').val();
+	var eventEnd = $('#eventEnd').val();
+
+	myDataRef.push({name:name,email:email,eventName:eventName,eventType:eventType,eventStart:eventStart,eventEnd:eventEnd});
+	myDataRef.on('child_added', function(snapshot) {
+  			var message = snapshot.val();
+  			displayChatMessage(message.name,message.email,message.eventName,message.eventType,message.eventStart,message.eventEnd);
+	});
+}
+
+var displayChatMessage = function(name,email,eName,eType,start,evEnd){
+	$('.okvir').append('<h1>'+name+'</h1>');
+	$('.okvir').append('<p>'+email+'</p>');
+	$('.okvir').append('<p>'+eName+'</p>');
+	$('.okvir').append('<p>'+eType+'</p>');
+	$('.okvir').append('<p>'+start+'</p>');
+	$('.okvir').append('<p>'+evEnd+'</p>');
+
+}
+
 $('.info').hide();
 $('.event').hide();
 $('.eventTable').hide();
@@ -17,10 +45,19 @@ $('.forward').click(function(){
 $('.lastOne').click(function(){
 	$('.eventTable').hide();
 	$('.event').hide(300);
+	$('.eventTable').show(300);
 })
 
 $('.showEvents').click(function(){
 	$('.event').hide(300);
 	$('.info').hide(300);
 	$('.eventTable').show(300);
+	getInfo();
 })
+
+
+
+
+
+
+//myDataRef.set('User ' + name + ' says ' + text);
