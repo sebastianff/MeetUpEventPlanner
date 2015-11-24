859b0ccaee96a411485824bec6fddb2bf78740d5
sebastianff <sebastianffarago@gmail.com>
2015-11-24 21:14:04 +0100

    Shaping up

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 $('.info').hide();
 $('.event').hide();
+$('.eventTable').hide();
+
 
 $('.start').click(function(){
+	$('.eventTable').hide();
 	$('.info').show(300);
 });
 $('.forward').click(function(){
 	$('.info').hide(300);
+	$('.eventTable').hide();
 	$('.event').show(300);
 })
 
 
-$('.forward').click(function(){
+$('.lastOne').click(function(){
+	$('.eventTable').hide();
 	$('.event').hide(300);
+})
+
+$('.showEvents').click(function(){
+	$('.event').hide(300);
+	$('.info').hide(300);
+	$('.eventTable').show(300);
 })
